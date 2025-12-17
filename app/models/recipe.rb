d1d7605c0bc2cc1e72758d7557e2da10bc5493af
han-techtech1020 <kishohatano@gmail.com>
2025-12-17 23:58:56 +0900

    アソシエーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  
+  # 中間テーブルとの関係
+  has_many :recipe_ingredients, dependent: :destroy
+  # 食材との多対多の関係（ここがポイント！）
+  has_many :ingredients, through: :recipe_ingredients
+
 end
