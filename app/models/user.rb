c0cc26cf093bfe4ed46d0175f05c205908d5b07c
han-techtech1020 <kishohatano@gmail.com>
2025-12-23 21:13:45 +0900

    userアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   # 「ユーザーは、たくさんの在庫を持っている」
+  has_many :stocks, dependent: :destroy
+  # レシピなども
+  has_many :recipes, dependent: :destroy
+  has_many :family_members, dependent: :destroy
 end
