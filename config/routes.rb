1a720c2e6892906c68954cc06d9f56604978d85b
han-techtech1020 <kishohatano@gmail.com>
2025-12-24 14:43:29 +0900

    AI用ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   root to: "stocks#index" # トップページを冷蔵庫の中身にする
   resources :stocks, only: [:index, :new, :create, :destroy]
   resources :family_members, only: [:index, :new, :create, :edit, :update, :destroy]
+  # AI提案用のルートを追加
+  resources :recipes, only: [:index, :create] do
+    collection do
+      get :generate # 「AIで提案する」画面
+    end
+  end
 end
