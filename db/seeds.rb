3bf6e0ed40880724bd5b154f00fac37666eb97eb
han-techtech1020 <kishohatano@gmail.com>
2025-12-24 15:07:14 +0900

    初期データ登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 既存データをクリア（開発用）
+Ingredient.destroy_all
+
+# 野菜
+Ingredient.create!([
+  { name: 'キャベツ', category: '野菜' },
+  { name: '人参', category: '野菜' },
+  { name: '玉ねぎ', category: '野菜' },
+  { name: 'じゃがいも', category: '野菜' }
+])
+
+# 肉
+Ingredient.create!([
+  { name: '豚肉', category: '肉' },
+  { name: '鶏肉', category: '肉' },
+  { name: '牛肉', category: '肉' }
+])
+
+# 調味料など
+Ingredient.create!([
+  { name: '醤油', category: '調味料' },
+  { name: '味噌', category: '調味料' },
+  { name: '卵', category: '卵・乳製品' }
+])
+
+puts "初期データの投入に成功しました！"
