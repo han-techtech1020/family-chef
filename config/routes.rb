9720d7413cf1f7ab9c0673aaf6623c69ef1a7449
han-techtech1020 <kishohatano@gmail.com>
2025-12-26 12:36:42 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "stocks#index" # トップページを冷蔵庫の中身にする
-  resources :stocks, only: [:index, :new, :create, :destroy]
+  resources :stocks, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :family_members, only: [:index, :new, :create, :edit, :update, :destroy]
   # AI提案用のルートを追加
   resources :recipes, only: [:index, :create] do
